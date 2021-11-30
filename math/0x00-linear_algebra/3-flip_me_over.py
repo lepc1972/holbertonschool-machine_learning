07bb8fdc7bba22c09d182977c9f3741ab2bb15d9
lepc1972 <vagrant@ubuntu>
2021-11-30 17:48:18 +0000

    transpose matrix

---
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -0,0 +1,8 @@
+# write a function  that returns the transpose of a matrix:
+
+
+def matrix_transpose(matrix):
+    """
+    Returns the transpose of a matrix.
+    """
+    return [list(i) for i in zip(*matrix)]
