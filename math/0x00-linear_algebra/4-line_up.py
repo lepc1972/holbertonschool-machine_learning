d0008a7989764a8dcb54791044c43956d3a477d3
lepc1972 <vagrant@ubuntu>
2021-11-30 21:32:36 +0000

    add two arrays

---
+++ math/0x00-linear_algebra/4-line_up.py
@@ -0,0 +1,10 @@
+# write a function that adds two arrays elements-wise
+
+
+def add_arrays(arr1, arr2):
+    """
+    Return the sum of two arrays
+    """
+    if len(arr1) != len(arr2):
+        return None
+    return [x + y for x, y in zip(arr1, arr2)]
