bc21ab11b87f2052ddcf6b3e75ef887b3ee8779f
lepc1972 <vagrant@ubuntu>
2021-12-02 18:59:29 +0000

    my scale

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Write a function def np_shape(matrix): that calculates the shape of a numpy.ndarray"""
+
+
+def np_shape(matrix):
+    """
+    Function that calculates the shape of a numpy.ndarray
+    Args:
+        matrix: numpy.ndarray
+    Returns:
+        tuple: shape of the matrix
+    """
+    return matrix.shape
