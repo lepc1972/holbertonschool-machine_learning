72a99a247df7988a8ce8cfdbc139ef8a1e9e0c18
lepc1972 <vagrant@ubuntu>
2021-12-03 13:19:30 +0000

    fix descriptions length

--- math/0x00-linear_algebra/14-saddle_up.py
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-"""Write a function def np_matmul(mat1, mat2): that performs matrix multiplication"""
+"""Write a function def np_matmul(mat1, mat2):
+that performs matrix multiplication"""
 import numpy as np
+
 
 def np_matmul(mat1, mat2):
     """Function that performs matrix multiplication"""
