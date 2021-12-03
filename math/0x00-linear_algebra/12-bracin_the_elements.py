4d153a66d9ec12521ed5c21fd2422f71026b4fa5
lepc1972 <vagrant@ubuntu>
2021-12-03 13:15:50 +0000

    fix descriptions

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-"""Write a function def np_elementwise(mat1, mat2): that performs element-wise addition, subtraction, multiplication, and division"""
+"""Write a function def np_elementwise(mat1, mat2):
+that performs element-wise addition, subtraction, multiplication,
+and division"""
 
 
 def np_elementwise(mat1, mat2):
-    """Return the element-wise addition, subtraction, multiplication, and division of two matrices"""
+    """Return the element-wise addition, subtraction, multiplication,
+    and division of two matrices"""
     return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
