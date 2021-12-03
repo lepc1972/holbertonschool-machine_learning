ab2c1786c37552f02ac3df5a2f67241848901752
lepc1972 <vagrant@ubuntu>
2021-12-03 02:16:59 +0000

    matrix multipl

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Write a function def np_matmul(mat1, mat2): that performs matrix multiplication"""
+import numpy as np
+
+def np_matmul(mat1, mat2):
+    """Function that performs matrix multiplication"""
+    return mat1.dot(mat2)
