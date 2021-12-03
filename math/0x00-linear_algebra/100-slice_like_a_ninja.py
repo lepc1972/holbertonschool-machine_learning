5cfbdc5a03e69a4ca7216054dccb74c2fda3037d
lepc1972 <vagrant@ubuntu>
2021-12-03 13:40:50 +0000

    advanced

--- math/0x00-linear_algebra/100-slice_like_a_ninja.py
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -3,7 +3,16 @@
 that slices a matrix along specific axes"""
 
 
+
 def np_slice(matrix, axes={}):
-    import numpy as np
-    """Write a function def np_slice(matrix, axes={}): that slices a matrix along specific axes"""
-    return matrix[tuple(axes.values())]
+    """Slices a matrix like a ninja"""
+    ax_values = {}
+    max_axis = max(axes.keys())
+
+    for j in range(max_axis):
+        ax_values[j] = (None, None, None)
+
+    for key in axes.keys():
+        ax_values[key] = axes[key]
+
+    return(matrix[tuple(slice(*ax_values[i]) for i in ax_values.keys())])
