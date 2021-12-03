f3ea7315e31f42b487eeaa0182881a5ef140135e
lepc1972 <vagrant@ubuntu>
2021-12-03 02:37:19 +0000

    tired

---
+++ math/0x00-linear_algebra/101-the_whole_barn.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module to add matrices
+"""
+
+
+def add_matrices(mat1, mat2):
+    """
+    function to add two matrices
+    """
+    if len(mat1) != len(mat2):
+        return None
+    return True
