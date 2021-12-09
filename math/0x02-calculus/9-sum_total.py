212565f7ebbbbd576027eeace86ca9b93b2e5878
lepc1972 <vagrant@ubuntu>
2021-12-09 02:14:00 +0000

    pep8

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-"""Write a function def summation_i_squared(n): that calculates [\sum_{i=1}^{n} i^2] :"""
+"""Write a function def summation_i_squared(n):
+that calculates [sum_{i=1}^{n} i^2] :"""
 
 
 def summation_i_squared(n):
-    """Function that calculates [\sum_{i=1}^{n} i^2]"""
+    """Function that calculates [sum_{i=1}^{n} i^2]"""
     if type(n) is not int or n < 1:
         return None
     return int(n * (n + 1) * (2 * n + 1) / 6)
