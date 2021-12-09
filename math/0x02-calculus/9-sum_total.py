aceba586f87599ef90ade63efa1f3b5fcd4ec680
lepc1972 <vagrant@ubuntu>
2021-12-09 02:09:50 +0000

    55 fix

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -3,12 +3,7 @@
 
 
 def summation_i_squared(n):
-    """Calculates the summation of the squares of the integers from 1 to n:
-    Args:
-        n: integer that represents the final value of the summation
-    Returns:
-        The summation of the squares of the integers from 1 to n
-    """
+    """Function that calculates [\sum_{i=1}^{n} i^2]"""
     if type(n) is not int or n < 1:
         return None
-    return sum([i**2 for i in range(1, n+1)])
+    return int(n * (n + 1) * (2 * n + 1) / 6)
