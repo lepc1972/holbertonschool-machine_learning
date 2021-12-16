15e0807fd0be2e6f7e174c156f437a191451303a
lepc1972 <vagrant@ubuntu>
2021-12-16 01:40:05 +0000

    corrections 2

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -1,49 +1,48 @@
 #!/usr/bin/env python3
-"""Create a class Poisson that represents a poisson distribution"""
+"""Poisson"""
 
 
 class Poisson:
-    """Poisson distribution"""
+    """Poisson class"""
 
     def __init__(self, data=None, lambtha=1.):
-        """Constructor"""
-        self.lambtha = float(lambtha)
-        if data is None:
+        """constructor"""
+        if data is not None:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) <= 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = float(sum(data) / len(data))
+        else:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
-            else:
-                self.lambtha = float(lambtha)
-        else:
-            if type(data) != list:
-                raise TypeError("data must be a list")
-            if len(data) < 2:
-                raise ValueError("data must contain multiple values")
-            self.lambtha = self.__calculate_lambtha(data)
-
-    def __calculate_lambtha(self, data):
-        """Calculate the value of lambtha"""
-        mean = sum(data) / len(data)
-        return mean
+            self.lambtha = float(lambtha)
 
     def pmf(self, k):
-        """Calculate the value of the pmf for a given number of successes"""
-        e = 2.7182818285
-        if type(k) != int:
-            raise TypeError("k must be an integer")
+        """Calculates the value of the PMF for a given number"""
+        k = int(k)
+
         if k < 0:
             return 0
+
+        # Multiply elements one x one
         factorial = 1
-        for i in range(1, k + 1):
-            factorial *= i
-        return (self.lambtha ** k) * (e ** -self.lambtha) / factorial
+        for x in range(1, k + 1):
+            factorial *= x
+
+        return ((2.7182818285 ** (- self.lambtha)) *
+                (self.lambtha ** k)) / factorial
 
     def cdf(self, k):
-        """Calculate the value of the cdf for a given number of successes"""
-        if type(k) != int:
-            raise TypeError("k must be an integer")
+        """Calculates the value of the CDF for a given number"""
+        k = int(k)
         if k < 0:
             return 0
-        cdf = 0
+
+        summation = 0
         for i in range(k + 1):
-            cdf += self.pmf(i)
-        return cdf
+            factorial = 1
+            for x in range(1, i + 1):
+                factorial *= x
+            summation += (self.lambtha ** i) / factorial
+        return (2.7182818285 ** (-self.lambtha)) * summation
